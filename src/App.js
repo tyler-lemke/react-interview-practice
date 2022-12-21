a0814c9fe7a6d0c1ea4e02dcd9199b7571c4fe7e
tylerslemke <tyleris@gmail.com>
2022-12-20 17:53:41 -0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./style.css";
 
 export default function App() 
-  .
+
+  // get the data from this api - https://picsum.photos/v2/list
+
+  // push that data into a list of Image Componentss
+
+  // each image component will display the author name and the image itself 
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
